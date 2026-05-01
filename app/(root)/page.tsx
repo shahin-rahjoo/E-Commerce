f614e5b15d082085b7dfb3e33dd8eecce82e09ef
shahin rahjoo <shahin.rahjoo2000@gmail.com>
2026-05-01 20:07:02 +0200

    product db added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
 const Homepage = () => {
-
-  return <>
-  {/* the ProductList below is sending data and title to the product-list component*/}
-  <ProductList data={sampleData.products} title={'Our Newest Collection'}/>
-  </>;
+  return (
+    <>
+      {/* the ProductList below is sending data and title to the product-list component*/}
+      <ProductList data={sampleData.products} title={"Our Newest Collection"} />
+    </>
+  );
 };
 
 export default Homepage;
